e83a999bc6207490d5aa3d821d9630339bc29932
Nidhisharora <nidhisharora79@gmail.com>
2026-01-29 00:19:43 +0530

    update js

--- projects/ComponentUI/script.js
+++ projects/ComponentUI/script.js
@@ -1,12 +1,17 @@
 const modal = document.getElementById("modal");
 const openModal = document.getElementById("openModal");
 const closeModal = document.getElementById("closeModal");
+const closeBtn = document.getElementById("closeBtn");
 
 openModal.addEventListener("click", () => {
   modal.style.display = "flex";
 });
 
 closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
